9c9f7af5a4ce0dc534d93c8f9a04fdff00143d4c
Marcos Wellington <marcoswoc@gmail.com>
2023-06-20 20:05:25 -0300

    Adicionando DbContext e PageExceptionFilter

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoDb>(optionsAction => optionsAction.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
