435b658efafd072749beec4293c175d82d17ec6b
Marcos Wellington <marcoswoc@gmail.com>
2023-06-20 20:13:57 -0300

    Adicionando HTTP GET Todo items is complete

--- Program.cs
+++ Program.cs
@@ -11,4 +11,7 @@ var app = builder.Build();
 app.MapGet("/todoitems", async (TodoDb db) =>
   await db.Todos.ToListAsync());
 
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+  await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
 app.Run();
