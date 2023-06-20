b9639b49d02df9b84aa5616ef03001faa03a2581
Marcos Wellington <marcoswoc@gmail.com>
2023-06-20 20:17:31 -0300

    Adicionando HTTP GET Todo item by id

--- Program.cs
+++ Program.cs
@@ -14,4 +14,10 @@ app.MapGet("/todoitems", async (TodoDb db) =>
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
   await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+  await db.Todos.FindAsync(id) 
+    is Todo todo 
+      ? Results.Ok(todo)
+      : Results.NotFound());
+
 app.Run();
