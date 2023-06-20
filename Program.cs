004632beae1dac73efddf57818ca5840756b7a51
Marcos Wellington <marcoswoc@gmail.com>
2023-06-20 20:10:37 -0300

    Adicionando HTTP GET Todo items

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TodoDb>(optionsAction => optionsAction.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/todoitems", async (TodoDb db) =>
+  await db.Todos.ToListAsync());
 
 app.Run();
